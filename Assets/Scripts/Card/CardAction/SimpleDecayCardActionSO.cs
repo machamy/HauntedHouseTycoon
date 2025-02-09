77d7b7cc8acad11f7120e119ae7bd53b136f45b5
machamy <macham2001@gmail.com>
2025-02-09 12:24:11 +0900

    docs : SimpleDecayCardActionSO 주석작업

--- Assets/Scripts/Card/CardAction/SimpleDecayCardActionSO.cs
+++ Assets/Scripts/Card/CardAction/SimpleDecayCardActionSO.cs
@@ -2,6 +2,10 @@
 
 using UnityEngine;
 
+/// <summary>
+/// 일정 턴이 지나면, 카드 비활성화(Action을 모두 제거)
+/// 체크 시점은 NPC의 턴이 끝날 때
+/// </summary>
 [CreateAssetMenu (menuName = "CardAction/SimpleDecayCardAction")]
 public class SimpleDecayCardActionSO : BaseCardActionSO
 {
@@ -22,7 +26,8 @@ public class SimpleDecayCardActionSO : BaseCardActionSO
         {
             Debug.Log($"[SimpleDecayCardActionSO::OnTurnExit] {room.name} : Card decayed({decayTurnRemain})");
             cardData.CleanAction();
-            room.FocusColor(new Color(0.4f, 0.4f, 0.4f, 0.5f));
+            // TODO: 디버그용 색변경, 스프라이트 등을 변경해야함
+            room.FocusColor(new Color(0.4f, 0.4f, 0.4f, 0.5f)); 
             return true;
         }
         return breakChain;

22d6da308e6ab7753779d2ba01cdacbd0735776b
choijeanwoo <jwoochoi0210@gmail.com>
2025-02-20 16:16:20 +0900

    ErrorModify

--- Assets/Scripts/DataBase/DataBase.cs
+++ Assets/Scripts/DataBase/DataBase.cs
@@ -1,4 +1,20 @@
-﻿public class DataBase
-{
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class DataBase : SingletonBehaviour<DataBase>
+{
+    [SerializeField] private List<CardDataSO> cardDataSOList;
+    public List<CardData> cardDataList;
+
+    private void OnValidate()
+    {
+        cardDataList = new List<CardData>();
+        foreach (var cardDataSO in cardDataSOList)
+        {
+            if (!cardDataSO || cardDataSO.cardData == null)
+                continue;
+            cardDataList.Add(cardDataSO.cardData);
+        }
+    }
 }

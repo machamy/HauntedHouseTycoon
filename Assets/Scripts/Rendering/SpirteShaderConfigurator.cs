f9f56eb5e7a95289285141adcf2ef56dc4dd4bc1
machamy <macham2001@gmail.com>
2025-03-04 20:48:21 +0900

    feat : 손님 스프라이트에도 적용

--- Assets/Scripts/Rendering/SpirteShaderConfigurator.cs
+++ Assets/Scripts/Rendering/SpirteShaderConfigurator.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -8,6 +9,7 @@ public class SpirteShaderConfigurator : MonoBehaviour
     [SerializeField] private bool setMaterial = false;
     [SerializeField] private Material material = null;
     [SerializeField] bool registerAllChildren = true;
+    [SerializeField] bool overrideChildren = false;
     [SerializeField] private SpriteRenderer[] spriteRenderers = Array.Empty<SpriteRenderer>();
     [SerializeField] private ShadowCastingMode shadowCastingMode = ShadowCastingMode.On;
 
@@ -16,7 +18,7 @@ public class SpirteShaderConfigurator : MonoBehaviour
         if (spriteRenderers.Length == 0)
         {
             if (registerAllChildren)
-                spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+                spriteRenderers = GetRenderers();
             else
                 spriteRenderers = new[] { GetComponent<SpriteRenderer>() };
         }
@@ -28,6 +30,36 @@ public class SpirteShaderConfigurator : MonoBehaviour
                 spriteRenderer.material = material;
             spriteRenderer.shadowCastingMode = shadowCastingMode;
             
+        }
+    }
+    
+    private List<SpriteRenderer> renderers = new List<SpriteRenderer>();
+    private Queue<Transform> serachQueue = new Queue<Transform>();
+    private SpriteRenderer[] GetRenderers()
+    {
+        renderers.Clear();
+        serachQueue.Enqueue(transform);
+        while (serachQueue.Count > 0)
+        {
+            GetRenderersRecursive(serachQueue.Dequeue(), !overrideChildren);
+        }
+        
+        return renderers.ToArray();
+    }
+    private void GetRenderersRecursive(Transform parentTransform, bool stopOnAnoteherComponent = false)
+    {
+        for (int i = 0; i < parentTransform.childCount; i++)
+        {
+            var child = parentTransform.GetChild(i);
+            if (stopOnAnoteherComponent && child.TryGetComponent<SpirteShaderConfigurator>(out var _))
+            {
+                continue;
+            }
+            if (child.TryGetComponent<SpriteRenderer>(out var renderer))
+            {
+                renderers.Add(renderer);
+            }
+            serachQueue.Enqueue(child);
         }
     }
 

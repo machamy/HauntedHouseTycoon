6d4457516d6cb7e814096ea3f86aedd3af70ac98
machamy <macham2001@gmail.com>
2025-03-04 20:29:17 +0900

    feat : 스프라이트 렌더러 그림자 적용

---
+++ Assets/Scripts/Rendering/SpirteShaderConfigurator.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SpirteShaderConfigurator : MonoBehaviour
+{
+    [SerializeField] private bool setMaterial = false;
+    [SerializeField] private Material material = null;
+    [SerializeField] bool registerAllChildren = true;
+    [SerializeField] private SpriteRenderer[] spriteRenderers = Array.Empty<SpriteRenderer>();
+    [SerializeField] private ShadowCastingMode shadowCastingMode = ShadowCastingMode.On;
+
+    public void ApplyConfig()
+    {
+        if (spriteRenderers.Length == 0)
+        {
+            if (registerAllChildren)
+                spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+            else
+                spriteRenderers = new[] { GetComponent<SpriteRenderer>() };
+        }
+        foreach (var spriteRenderer in spriteRenderers)
+        {
+            if(!spriteRenderer)
+                return;
+            if(material && setMaterial)
+                spriteRenderer.material = material;
+            spriteRenderer.shadowCastingMode = shadowCastingMode;
+            
+        }
+    }
+
+    private void Awake()
+    {
+        ApplyConfig();
+    }
+
+
+    private void OnValidate()
+    {
+        ApplyConfig();
+    }
+}
